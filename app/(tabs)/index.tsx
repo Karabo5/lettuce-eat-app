518a2297b411e6a816cdfe7977c2b7513506a91a
Karabo Mogano <karabomogano27@gmail.com>
2026-01-14 23:51:10 +0200

    Added a feature home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,8 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import HomeScreen from '@/screens/HomeScreen'
 
 export default function index() {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <HomeScreen/>
   )
 }
---
+++ theme/colors.ts
@@ -0,0 +1,10 @@
+export const COLORS = {
+  primary: '#7BAE3E',
+  darkGreen: '#3E5F1C',
+  background: '#F9FAF7',
+  gray: '#343232',
+  text: '#1E1E1E',
+  white: '#FFFFFF',
+  brown: '#7c4d2b', 
+};
+
